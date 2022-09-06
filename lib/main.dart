f161f40e2b1d9838e36710638ff5b72b3991020e
FerdiMauladi <56813073+FerdiMauladi@users.noreply.github.com>
2022-09-06 16:24:37 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,12 @@ class HexaPlus extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< Updated upstream
-      title: 'HexaPlus',
       initialRoute: HomeScreen.route,
       routes: {
         HomeScreen.route: (context) => const HomeScreen(),
         CompanyScreen.route: (context) => const CompanyScreen(),
       },
-=======
       title: 'Hexa Smart',
-      home: HomeScreen(),
->>>>>>> Stashed changes
     );
   }
 }
