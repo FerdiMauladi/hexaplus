2c27cc23bb7d98a1cd15954e5196924af0e6762b
FerdiMauladi <56813073+FerdiMauladi@users.noreply.github.com>
2022-09-15 13:33:12 +0700

    update screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,49 +17,51 @@ class HexaPlus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Yorkten',
-      ),
-      initialRoute: HomeScreen.route,
-      routes: {
-        HomeScreen.route: (context) => const HomeScreen(),
-        CompanyScreen.route: (context) => const CompanyScreen(),
-        ProductScreen.route: (context) => const ProductScreen(),
-        HowToBuyScreen.route: (context) => const HowToBuyScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == HomeScreen.route) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return const HomeScreen();
-            },
-          );
-        } else if (settings.name == CompanyScreen.route) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return const CompanyScreen();
-            },
-          );
-        } else if (settings.name == ProductScreen.route) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return const ProductScreen();
-            },
-          );
-        } else if (settings.name == HowToBuyScreen.route) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return const HowToBuyScreen();
-            },
-          );
-        }
+    return Semantics(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Yorkten',
+        ),
+        initialRoute: HomeScreen.route,
+        routes: {
+          HomeScreen.route: (context) => const HomeScreen(),
+          CompanyScreen.route: (context) => const CompanyScreen(),
+          ProductScreen.route: (context) => const ProductScreen(),
+          HowToBuyScreen.route: (context) => const HowToBuyScreen(),
+        },
+        onGenerateRoute: (settings) {
+          if (settings.name == HomeScreen.route) {
+            return MaterialPageRoute(
+              builder: (context) {
+                return const HomeScreen();
+              },
+            );
+          } else if (settings.name == CompanyScreen.route) {
+            return MaterialPageRoute(
+              builder: (context) {
+                return const CompanyScreen();
+              },
+            );
+          } else if (settings.name == ProductScreen.route) {
+            return MaterialPageRoute(
+              builder: (context) {
+                return const ProductScreen();
+              },
+            );
+          } else if (settings.name == HowToBuyScreen.route) {
+            return MaterialPageRoute(
+              builder: (context) {
+                return const HowToBuyScreen();
+              },
+            );
+          }
 
-        assert(false, 'Need to implement ${settings.name}');
-        return null;
-      },
-      title: 'Hexa Smart',
+          assert(false, 'Need to implement ${settings.name}');
+          return null;
+        },
+        title: 'Hexa Smart',
+      ),
     );
   }
 }
