51c380996e6b5f88622688722c680c4b92943093
FerdiMauladi <56813073+FerdiMauladi@users.noreply.github.com>
2022-10-05 21:12:25 +0700

    update feature mobile

--- lib/screen/component/footer.dart
+++ lib/screen/component/footer.dart
@@ -191,7 +191,9 @@ class Footer extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         GestureDetector(
-                          onTap: () {},
+                          onTap: () {
+                            Navigator.of(context).pushNamed(FaqScreen.route);
+                          },
                           child: const Text(
                             'FAQ',
                             style: TextStyle(
@@ -202,6 +204,15 @@ class Footer extends StatelessWidget {
                         Container(
                           margin: const EdgeInsets.only(top: 10.0),
                           child: GestureDetector(
+                            onTap: () async {
+                              final Uri url = Uri.parse(
+                                  'https://api.whatsapp.com/send?phone=6285171717367&text=Halo%20Mincan!%0ASaya%20ingin%20bertanya%20tentang%20produk%20dan%20jasa%20Hexa!%20');
+                              if (await canLaunchUrl(url)) {
+                                await launchUrl(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
                             child: const Text(
                               'Kontak Kami',
                               style: TextStyle(
--- lib/screen/faq/faqmobile_screen.dart
+++ lib/screen/faq/faqmobile_screen.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../model/question_model.dart';
 import '../component/drawer.dart';
 import '../component/footer.dart';
 import '../home/home_screen.dart';
+import 'faq_view_model.dart';
 
-class FaqMobileScreen extends StatelessWidget {
+class FaqMobileScreen extends StatefulWidget {
   const FaqMobileScreen({Key? key}) : super(key: key);
 
   @override
+  State<FaqMobileScreen> createState() => _FaqMobileScreenState();
+}
+
+class _FaqMobileScreenState extends State<FaqMobileScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      var viewModel = Provider.of<FaqViewModel>(context, listen: false);
+
+      await viewModel.getData().then(
+          (_) => viewModel.listDataQuestionModel = viewModel.listQuestions);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final controller = Provider.of<FaqViewModel>(context);
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       drawer: const DrawerScreen(),
       appBar: AppBar(
@@ -43,8 +65,116 @@ class FaqMobileScreen extends StatelessWidget {
       ),
       body: ListView(
         scrollDirection: Axis.vertical,
-        children: const [
-          Footer(),
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: width * 0.1,
+              right: width * 0.2,
+              top: height * 0.05,
+              bottom: height * 0.05,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: width < 980
+                      ? width < 654
+                          ? width * 0.4
+                          : width * 0.3
+                      : width * 0.2,
+                  child: const Text(
+                    'Frequently Asked Questions',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 15.0,
+                  ),
+                  color: Colors.white,
+                  width: width < 980
+                      ? width < 654
+                          ? width * 0.8
+                          : width * 0.6
+                      : width * 0.4,
+                  child: TextField(
+                    onChanged: (dataSearch) {
+                      controller.listDataQuestionModel =
+                          controller.listQuestions.where(
+                        (dataPertanyaan) {
+                          return (dataPertanyaan.answer!
+                                  .toLowerCase()
+                                  .contains(dataSearch.toLowerCase()) ||
+                              dataPertanyaan.question!
+                                  .toLowerCase()
+                                  .contains(dataSearch.toLowerCase()));
+                        },
+                      ).toList();
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(
+                        Icons.search,
+                      ),
+                      hintText: 'Search your questions, answer etc',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 50.0,
+                    bottom: 50.0,
+                  ),
+                  child: ExpansionPanelList(
+                    expansionCallback: (panelIndex, isExpanded) {
+                      setState(() {
+                        controller.listDataQuestion[panelIndex].isExpand =
+                            !isExpanded;
+                      });
+                    },
+                    children: controller.listDataQuestion.map<ExpansionPanel>(
+                      (QuestionModel questionModel) {
+                        return ExpansionPanel(
+                          headerBuilder: (context, isExpanded) {
+                            return ListTile(
+                              title: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  questionModel.question!,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          body: ListTile(
+                            title: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                questionModel.answer!,
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                          isExpanded: questionModel.isExpand,
+                        );
+                      },
+                    ).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Footer(),
         ],
       ),
     );
